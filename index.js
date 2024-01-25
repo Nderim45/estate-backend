d91fb50e1693a6b94bebd9786becf1b94aa27b96
unknown <nderim258@gmail.com>
2024-01-25 17:11:37 +0100

    Create middleware and error function

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/user", userRouter);
-
-app.use("/api/auth", authRouter);
-
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
