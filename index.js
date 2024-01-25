f07a3c33ef1738227a70fb186f496c95a71d66ca
unknown <nderim258@gmail.com>
2024-01-25 15:06:11 +0100

    Connected to DB

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@real-estate.jmi53vb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
 
 const app = express();
 
