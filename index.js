48fa2872819f93d138a26701ddda3f6c4747af7d
unknown <nderim258@gmail.com>
2024-01-25 16:49:56 +0100

    Create sign up api route

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/user-route.js";
+import authRouter from "./routes/auth-route.js";
 
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@real-estate.jmi53vb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-  )
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@real-estate.jmi53vb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+);
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+
+app.use("/api/auth", authRouter);
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
