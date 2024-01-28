3f779a8caf8101bcb3b93613c85c07e5eb405957
unknown <nderim258@gmail.com>
2024-01-28 17:59:10 +0100

    Created update user route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import userRouter from "./routes/user-route.js";
 import authRouter from "./routes/auth-route.js";
+import cookieParser from "cookie-parser";
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@real-estate.jmi53vb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
@@ -14,6 +15,8 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use(cookieParser());
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
@@ -24,6 +27,7 @@ app.options("*", (req, res, next) => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use((err, req, res, next) => {
+  console.log(err);
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
