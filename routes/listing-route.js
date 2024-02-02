e50b96b017fb7b5b1aa12772fb4fd605fc0698e1
unknown <nderim258@gmail.com>
2024-02-02 16:36:26 +0100

    Created get listing route

--- routes/listing-route.js
+++ routes/listing-route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { createListing, deleteListing, updateListing } from "../controllers/listing-controler.js";
+import {
+  createListing,
+  deleteListing,
+  getListing,
+  updateListing,
+} from "../controllers/listing-controler.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -9,5 +14,7 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 
 router.post("/update/:id", verifyToken, updateListing);
+
+router.get("/:id", getListing);
 
 export default router;
